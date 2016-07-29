b77657c2c0c91dc1a3dfa1e35de14b3d830884a5
Jeonggyu Park <goodsogi@gmail.com>
2016-07-29 09:19:44 +0900

    first commit

---
+++ gen/mcore/edu/test/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package mcore.edu.test;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/mcore/edu/test/implementation/ExtendApplication.java
@@ -0,0 +1,9 @@
+package mcore.edu.test.implementation;
+
+import m.client.android.library.core.common.MorpheusApplication;
+
+public class ExtendApplication extends MorpheusApplication {
+	public ExtendApplication() {
+
+	}
+}
